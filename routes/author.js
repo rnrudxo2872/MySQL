c91059d1644076390b187490979a92f807465f7b
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-07-22 16:13:32 +0900

    저자 추가 기능

--- routes/author.js
+++ routes/author.js
@@ -22,11 +22,50 @@ router.get('', (req, res) => {
         <p>
         ${template.author_list_table(authors)}
         </p>
+        <p>
+        <a href="/author/create" style="color: lightgrey;border:1px solid aquamarine;
+        border-radius:10%;background-color: steelblue;opacity: 0.5;">
+        저자 추가
+        </a>
+        </p>
         </div>
         `)
         console.log(sanitizedTitle)
         res.send(html)
       })
+    })
+});
+
+router.get('/create',(req,res) =>{
+    db.query('select * from topic', function (error, topics) {
+        if (error) {
+          throw error;
+        }
+          title = `Author Add`;
+          list = template.list(topics, `<a href="/topic/page_create">create</a>`);
+    
+          let sanitizedTitle = sanitizeHtml(title);
+  
+          var html = template.HTML(sanitizedTitle, list, `
+          <div id="article">
+          <h2>${sanitizedTitle}</h2>
+          <p>
+          ${template.author_create()}
+          </p>
+          </div>
+          `)
+          console.log(sanitizedTitle)
+          res.send(html)
+      })
+});
+
+router.post('/create_process',(req,res) => {
+    var post = req.body;
+
+    db.query(`INSERT INTO author (author, profile) 
+    VALUES(?, ?)`, [post.author, post.profile], function (error, result) {
+      res.redirect(302, `/author`) //redirect과정 = res.writeHead(302, {Location: `/page/${title}) 
+      res.end();
     })
 });
 
