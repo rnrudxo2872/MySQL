baf6b0671f9e2135b6ccfd5e216ff9fa2fc2ff77
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-08-03 18:23:12 +0900

    search Type 추가

--- routes/serch.js
+++ routes/serch.js
@@ -6,13 +6,21 @@ var db = require('../lib/db.js');
 router.get('', (req, res) => {
     let word = req.query.title;
     //word = '%' + word + '%'
-
+    let Type = "";
+    if(req.query.searchType === "title+body"){
+        Type =`where title REGEXP ? OR description REGEXP '${word}'`; 
+    }
+    else if(req.query.searchType === "title"){
+        Type = `where title REGEXP ?`;
+    }
+    else if(req.query.searchType === "body"){
+        Type = `where description REGEXP ?`;
+    }
     db.query('SELECT id,title FROM topic', function (error, topics) {
         let ds = db.query(`SELECT topic.id AS topic_id,title,created,author.id AS author_id,author 
-                            FROM (select * from topic where title REGEXP ?)topic LEFT JOIN author 
+                            FROM (select * from topic ${Type})topic LEFT JOIN author 
                             ON topic.author_id = author.id;`, [word], function (serch_error, result) {
-
-            console.log(ds.sql);
+            console.log(result);
             let list = template.list(topics, '<a href="/topic/page_create">create</a>');
             var html = template.control_HTML('', list, `<div id="article">
             <h2>검색 결과</h2>
